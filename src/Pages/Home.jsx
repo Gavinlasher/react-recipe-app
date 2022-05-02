fb2b49fbee6e16f9bb77719fc1a5e5426e701039
gavinlasher <krgavin21@yahoo.com>
2022-05-01 21:50:27 -0600

    commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Veggie from "../componets/Veggie"
+import Popular from "../componets/Popular"
+export default function Home() {
+  return (
+    <div>
+<Popular/>
+<Veggie/>
+
+    </div>
+  )
+}
